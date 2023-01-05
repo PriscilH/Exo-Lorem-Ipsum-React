a1b9e10e967a9b742e705419e053672a18f017cf
Priscilla <comptepro@mbpdepriscilla.home>
2023-01-05 12:54:55 +0100

    add components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+// import Title from "./components/Title";
+// import Description from './components/Description';
+import Content from './components/Content';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      	
+    <Content title={"Lorem"} 
+    description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam non neque in eros imperdiet ornare et vel mauris. Praesent faucibus tellus in mattis semper. Nullam eleifend lorem nulla, feugiat imperdiet ex dapibus sed. Integer lobortis dolor sed arcu rhoncus, vitae facilisis lacus ornare. Vestibulum quis orci nec velit blandit fringilla eget eu nunc. Sed erat est, pulvinar posuere mi et, bibendum elementum magna. Vivamus eu mauris consequat, venenatis velit quis, dignissim nunc. Phasellus id sagittis tellus, eu cursus dui. Vestibulum eu dictum est, eget malesuada arcu. Ut eu luctus sem, in rutrum justo. Nam ut odio orci. Vestibulum pretium nisl a maximus ultrices. Etiam tincidunt mi ipsum, id ullamcorper nibh placerat condimentum."}/>
+    <Content title={"Ipsum"}
+    description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam non neque in eros imperdiet ornare et vel mauris. Praesent faucibus tellus in mattis semper. Nullam eleifend lorem nulla, feugiat imperdiet ex dapibus sed. Integer lobortis dolor sed arcu rhoncus, vitae facilisis lacus ornare. Vestibulum quis orci nec velit blandit fringilla eget eu nunc. Sed erat est, pulvinar posuere mi et, bibendum elementum magna. Vivamus eu mauris consequat, venenatis velit quis, dignissim nunc. Phasellus id sagittis tellus, eu cursus dui. Vestibulum eu dictum est, eget malesuada arcu. Ut eu luctus sem, in rutrum justo. Nam ut odio orci. Vestibulum pretium nisl a maximus ultrices. Etiam tincidunt mi ipsum, id ullamcorper nibh placerat condimentum.Nulla tincidunt lobortis neque eu pretium. Etiam vehicula magna massa, nec posuere tellus vulputate quis. Nullam mattis laoreet dictum. Nulla eget magna in mauris accumsan pellentesque. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sit amet venenatis nunc. Cras vulputate, lacus ut porta auctor, libero massa fermentum lacus, ac posuere dolor nunc in massa. Donec suscipit justo nec metus tempus, sed ornare elit hendrerit. Suspendisse dapibus ut lorem eu euismod. Duis pulvinar lacus ac ante consectetur maximus. Nullam a odio suscipit, malesuada nulla et, euismod risus. Proin sem odio, finibus sed velit at, consequat sagittis felis. Sed commodo, odio non interdum semper, orci diam dictum felis, id sodales urna sem nec felis."} />
     </div>
+    
   );
 }
 
